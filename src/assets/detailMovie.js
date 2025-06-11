68a67570680d98406c776728295e908c826ad87a
deni mubarok hilmi <144646182+denimubarokhilmi@users.noreply.github.com>
2025-06-11 15:49:45 +0700

    Update detailMovie.js

--- src/assets/detailMovie.js
+++ src/assets/detailMovie.js
@@ -2,21 +2,33 @@ import index from "./index.js"
 import { ref } from "vue";
 const movie_id = ref([]);
 async function director(id) {
-    const res = await index.callAPi(`https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`)
-    const hasil = await res.json();
-    const finds = hasil.crew.find(element => element.job == 'Director');
-    return finds;
+    try {
+        const res = await index.callAPi(`https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`)
+        const hasil = await res.json();
+        const finds = hasil.crew.find(element => element.job == 'Director');
+        return finds;
+    } catch (error) {
+        throw error;
+    }
 }
 async function detailMovies(id) {
-    const response = await index.callAPi(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
-    const result = await response.json();
-    return result;
+    try {
+        const response = await index.callAPi(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        throw error;
+    }
 }
 async function getTrailler(id) {
-    const response = await index.callAPi(`https://api.themoviedb.org/3/movie/${id}/videos`);
-    const result = await response.json();
-    const finds = result.results.find(el => el.site == "YouTube");
-    return finds;
+    try {
+        const response = await index.callAPi(`https://api.themoviedb.org/3/movie/${id}/videos`);
+        const result = await response.json();
+        const finds = result.results.find(el => el.site == "YouTube");
+        return finds;
+    } catch (error) {
+        throw error;
+    }
 }
 export {
     director, detailMovies, movie_id, getTrailler,
