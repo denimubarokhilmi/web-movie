6db460cdb941bcf4db74145685810c192844e82a
deni mubarok hilmi <144646182+denimubarokhilmi@users.noreply.github.com>
2025-06-11 14:16:20 +0700

    Update compMovie.vue

--- src/components/compMovie.vue
+++ src/components/compMovie.vue
@@ -50,16 +50,17 @@ const pathAPI = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=
   }
 })();
 async function BtnMoreMovie(ev) {
+    ev.target.parentElement.previousElementSibling.insertAdjacentHTML(
+    "afterend",
+    index.createSpinner()
+  );
+  ev.target.disabled = true;
   const res = await index.callAPi(
     `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${count.value}`
   );
   const result = await res.json();
   count.value++;
-  ev.target.parentElement.previousElementSibling.insertAdjacentHTML(
-    "afterend",
-    index.createSpinner()
-  );
-  ev.target.disabled = true;
+
   setTimeout(() => {
     const spiner = document.querySelector(".create-spinner");
     ev.target.disabled = false;
