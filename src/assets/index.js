0b12f1e265ef8e1572118df7665e5e2471b89b3e
deni mubarok hilmi <144646182+denimubarokhilmi@users.noreply.github.com>
2025-06-11 11:25:10 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ const callAPi = (url) => {
     return fetch(url, {
         method: "GET",
         headers: {
-            Authorization: key,
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNGIxOGM4ZmM5MGI2ZTU3ZGNiMGE4MGZkZTY5OTAzNyIsIm5iZiI6MTc0MTM4OTMyMC45NjUsInN1YiI6IjY3Y2I3ZTA4ZTk0NjFhZGY0MTRmNjc5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VAo_I8JdV5zFde1DAHLZg9yDioXQ5tTPM0ZwFqq6kNE',
             Accept: "application/json",
         },
     });
