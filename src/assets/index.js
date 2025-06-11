1be9a92502e32717d6822a9707c1f3838a95e618
deni mubarok hilmi <144646182+denimubarokhilmi@users.noreply.github.com>
2025-06-11 15:50:10 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,21 @@
-const key = import.meta.env.VITE_ApiKeyFromTMDB;
 import { ref } from "vue";
-const callAPi = (url) => {
-    return fetch(url, {
-        method: "GET",
-        headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNGIxOGM4ZmM5MGI2ZTU3ZGNiMGE4MGZkZTY5OTAzNyIsIm5iZiI6MTc0MTM4OTMyMC45NjUsInN1YiI6IjY3Y2I3ZTA4ZTk0NjFhZGY0MTRmNjc5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VAo_I8JdV5zFde1DAHLZg9yDioXQ5tTPM0ZwFqq6kNE',
-            Accept: "application/json",
-        },
-    });
+const callAPi = async (url) => {
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNGIxOGM4ZmM5MGI2ZTU3ZGNiMGE4MGZkZTY5OTAzNyIsIm5iZiI6MTc0MTM4OTMyMC45NjUsInN1YiI6IjY3Y2I3ZTA4ZTk0NjFhZGY0MTRmNjc5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VAo_I8JdV5zFde1DAHLZg9yDioXQ5tTPM0ZwFqq6kNE',
+                Accept: "application/json",
+            },
+        });
+        if (!response.ok) {
+            throw response
+        };
+        return response;
+
+    } catch (error) {
+        throw error;
+    }
 };
 const createSpinner = () => {
     return `<div class="create-spinner text-center mt-2">
@@ -16,7 +24,6 @@ const createSpinner = () => {
       </div>`
 };
 const resultSearchMovie = ref([]);
-
 
 const useSearchMovie = () => {
     return resultSearchMovie;
