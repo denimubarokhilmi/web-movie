2faae314647abba56fc1a909259b52a029cf0cac
deni mubarok hilmi <144646182+denimubarokhilmi@users.noreply.github.com>
2025-06-14 19:02:42 +0700

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import movieDetail from "@/components/movie-detail.vue";
 import movie from "@/views/movie.vue";
 import tvShow from "@/views/tvShow.vue";
 import TvShowDetail from "@/components/TvShow-detail.vue";
+import compExample from "@/components/compExample.vue";
 const routes = [
     {
         path: '/', component: home, meta: {
@@ -34,6 +35,11 @@ const routes = [
     {
         path: '/TvShow-detail', component: TvShowDetail, meta: {
             title: "MovieFLx - TvShow - detail"
+        }
+    },
+    {
+        path: '/compExample', component: compExample, meta: {
+            title: 'MovieFlx - Example'
         }
     }
 ];
